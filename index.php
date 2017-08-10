3b281518927f43c9253072219e9b1dbecd387486
Andrii_Buda <andrii.soft.gr@gmail.com>
2017-08-10 17:18:39 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,469 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Bud.Andr
+ * Date: 09.08.2017
+ * Time: 13:41
+ */
+
+$string = 'Airstream
+Coachmen
+Dutchmen
+Fleetwood
+Forest River
+Gulf Stream
+Heartland
+Holiday Rambler
+Itasca
+Jayco
+Keystone
+Lance
+Monaco
+Newmar
+Roadtrek
+Thor Motor Coach
+Tiffin Motorhomes
+Winnebago
+A-Liner
+Adirondack
+Adventure Manufacturing
+Adventurer Lp
+Aerolite
+Airstream
+Alfa
+Allen Camper
+Alpenlite
+Alpha Ideal
+Alpine
+Ameri-Camp
+American Coach
+American Cruiser
+American Dream
+American Hauler
+American Star
+Anderson
+Aristocrat
+Aspen
+Atc - Aluminum Trailer Company
+Athens Park Homes
+Augusta Rv
+Auto Mate
+Avion
+Beaver
+Big Bite
+Bigfoot
+Bison
+Blue Bird
+Boles Aero
+Born Free
+Breckenridge
+Bridgeview
+Bundutecusa
+Canterbury
+Carriage
+Carson Trailer
+Casa Villa
+Casita
+Cavco
+Chalet Rv
+Champion
+Chariot
+Chariot Eagle
+Chevrolet
+Chinook
+Cikira
+Cirrus
+Coach House
+Coachmen
+Cobra
+Coleman
+Columbia Northwest
+Concord
+Cortez
+Country Coach
+Cozy Traveler
+Cree
+Cricket Trailer
+Crossroads
+Cruiser Rv Corp
+Ct Coachworks
+Custom
+DRV
+Damon
+Diamond
+Dodge
+Dolphin
+Dune Sport
+Dutch Park
+Dutchmen
+Dynamax Corp
+Earthbound
+Earthroamer
+Echo
+Eclipse Recreational Vehicles
+El Dorado
+Entegra Coach
+Era Motor Homes
+Escape Trailer Industries
+Establishment
+Europa
+Evergreen
+Excel - Peterson
+Extreme
+Extreme Warrior
+Ez Traveler
+FMC
+Fairmont Homes
+Featherlight Trailers
+Featherlite Coaches
+Fiber-Lite
+Firan Motor Coach
+Flagstaff
+Fleetwood
+Ford
+Forest River
+Foretravel
+Four Wheel Campers
+Franklin
+Freightliner
+Frontier
+GMC
+General Coach
+Genesis Supreme Rv
+Georgie Boy
+Glacier Bay
+Glacier Ice House
+Glendale
+Grand Design
+Great West Vans
+Gulf Stream
+Hallmark
+Happy Trails
+Harney Coach Works
+Haulmark
+Hawkins Motor Coach
+Heartland
+Heritage
+Hi-Lo
+Highland Ridge
+Holiday Coach
+Holiday Rambler
+Honey Coach Works
+Honorbuilt
+Horizon
+Host
+Hy-Line
+Hymer
+Ice Castle Fish Houses
+Instant Mobile House
+Intech
+Itasca
+Jayco
+K-Z
+Keystone
+Kibbi
+King Of The Road
+Kit
+Komfort
+Kropf
+Kustom Koach
+Lakota
+Lance
+Lazy Daze
+Legacy
+Leisure Travel
+Liberty Coach
+Lifestyle Luxury Rv
+Little Guy
+Livin\' Lite
+Luxury By Design
+MCI
+MVP
+Majestic Leisure Craft
+Mandalay Coach
+Marathon
+Mckenzie
+Mercedes-Benz
+Micro-Lite
+Midwest Automotive Designs
+Mirage
+Mobile Scout
+Monaco
+Mountain View
+National
+New Horizon
+New Horizons
+Newell Coach
+Newmar
+Nexus
+Nomad
+North American Traveler
+Northern Lite
+Northland Industries
+Northstar
+Northwood Mfg
+Nucamp
+Nuwa
+Odessa Industries
+Okanagan
+Omega
+Open Range
+Opus
+Other
+Outdoors Rv Manufacturing
+Outfitter
+Overland
+Pace American
+Pace-Arrow
+Pacific Coachworks
+Palomino
+Parkliner
+Pastime
+Phaeton
+Phoenix Usa
+Pilgrim International
+Pinnacle
+Pioneer
+Play-Mor
+Pleasure Way
+Potomac
+Prevost
+Prime Time
+Prolight Rv
+R-Vision
+Rage\'N
+Recreation By Design
+Red-E-Kamp
+Redwood
+Regency Conversions
+Renegade
+Rexhall
+Riverside Rv
+Riverside Trailer
+Roadmaster
+Roadtrek
+Runaway Campers
+Rv Camper
+S And S
+Safari
+Scamp
+Schult
+Seahawk
+Serro Scotty
+Shasta
+Show Hauler
+Silver Eagle
+Six-Pac
+Skamper
+Skyline
+Snowriver
+So-Cal Teardrops
+Somerset Rv
+Sportsmobile
+Starcraft
+Sun Lite
+Sun Valley
+Sundowner
+Sunlight
+Sunline
+Sunnybrook
+Sunrader
+Sunray
+Sunset Creek
+Sunset Park & Rv Inc.
+Sunvalley
+Sylvansport
+T@B
+T@G
+Taxa
+Terry
+Teton
+Thor Motor Coach
+Tiffin Motorhomes
+Trail Creek
+Trail Sport
+Trailmanor
+Travel Lite
+Travel Supreme
+Travel Villa
+Trekker Trailers
+Triple E
+Trophy Homes
+United Specialties
+Utah
+Vanleigh
+Venture Rv
+Veri-Lite
+Viking
+Vintage Trailers
+Vision
+Vogue
+Volkswagen
+Vrv
+Warrior Lifestyles
+Weekend Warrior
+West Coast Trailers
+Westcoast Leisure
+Western Rv
+Western Wilderness
+Winnebago
+Winnebago Eurovan Camper
+Wolf Creek
+Woodland Park
+Xplorer
+Yellowstone
+Yetti';
+
+$string2 = 'Absolute (204)
+Albemarle (144)
+Amel (144)
+Apreamare (167)
+Astondoa (250)
+Atlantis (173)
+Azimut (1558)
+Bavaria (1527)
+Bayliner (1295)
+Beneteau (3081)
+Benetti (200)
+Bertram (530)
+Boston Whaler (1216)
+C&C (190)
+Canados (150)
+Carver (987)
+Catalina (881)
+Century (138)
+Chaparral (756)
+Chris Craft (369)
+Chris-Craft (506)
+Cobalt (314)
+Contender (186)
+Cranchi (710)
+Crownline (262)
+Cruisers Yachts (555)
+Custom (276)
+Dehler (165)
+Dufour (670)
+Edgewater (184)
+Elan (370)
+Everglades (217)
+Fairline (1022)
+Ferretti (311)
+Ferretti Yachts (614)
+Formula (725)
+Fountain (242)
+Fountaine Pajot (341)
+Four Winns (560)
+Galeon (171)
+Glastron (151)
+Grady-White (435)
+Grand Banks (407)
+Grand Soleil (211)
+Hallberg-Rassy (247)
+Hanse (431)
+Hatteras (717)
+Hinckley (149)
+Hunter (882)
+Intrepid (191)
+Island Packet (267)
+J Boats (202)
+Jeanneau (2517)
+Key West (176)
+Lagoon (549)
+Larson (210)
+Leopard (144)
+Luhrs (285)
+Mainship (260)
+Maiora (192)
+Maxum (376)
+Meridian (215)
+Monterey (518)
+Moody (251)
+Nautor (143)
+Ocean Alexander (151)
+Ocean Yachts (171)
+Pearson (220)
+Pershing (522)
+Prestige (242)
+Princess (1267)
+Pursuit (522)
+Regal (813)
+Regulator (179)
+Rinker (501)
+Riva (661)
+Riviera (370)
+Robalo (354)
+Rodman (201)
+Sabre (178)
+Sailfish (171)
+Sanlorenzo (266)
+Scout Boats (312)
+Sea Fox (176)
+Sea Hunt (198)
+Sea Pro (146)
+Sea Ray (5076)
+Sealine (597)
+Sessa (284)
+Silverton (582)
+Sunseeker (1481)
+Tartan (216)
+Tiara (634)
+Tidewater (154)
+Trojan (147)
+Viking (548)
+Wellcraft (491)
+Westerly (204)
+X-Yachts (305)
+Yamaha (249)';
+
+$string3 = 'Aluminum
+Carbon Fiber
+Composite
+Ferro-Cement
+Fiberglass
+Hypalon
+Other
+PVC
+Steel
+Wood';
+
+$arr = explode(PHP_EOL, $string3);
+
+f3($arr);
+
+
+/**
+ * Simple function to print associative list
+ *
+ * @param $array
+ */
+function f1($array) {
+    foreach ($array as $delta => $item) {
+        echo $delta . '|' . $item . "<br>";
+    }
+}
+
+/**
+ * Delete Count Number with ()
+ *
+ * @param $array
+ */
+function f2($array) {
+    foreach ($array as $delta => $item) {
+        $item = preg_replace('/\(.*\)/', '', $item);
+        echo $delta . '|' . $item . "<br>";
+    }
+}
+
+/**
+ * Creates list with lowercased values
+ *
+ * @param $array
+ */
+function f3($array) {
+    foreach ($array as $delta => $item) {
+        $item = str_replace(' ', '_', $item);
+        $item = str_replace('-', '_', $item);
+        $delta = strtolower($item);
+        echo $delta . '|' . $item . "<br>";
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,107 @@
+/**
+ * Created by Bud.Andr on 10.08.2017.
+ */
+
+var erase = document.getElementById('delete');
+var submit = document.getElementById('calculate');
+
+submit.onclick = function (event) {
+    event.preventDefault();
+
+    var input = {
+        budget : document.getElementById('budget').value,
+        k      : document.getElementById('k').value,
+        w_rate : document.getElementById('w_rate').value,
+        n_bets : document.getElementById('n_bets').value,
+        a_bet  : document.getElementById('a_bet').value
+    };
+
+
+    w_rate = (+input.w_rate)/100;
+    console.log(w_rate);
+    var bets = input.n_bets;
+
+    for (var i = 0; i < bets; i++) {
+        if (Math.random() < w_rate) {
+            // console.log('Win');
+            generateRow(input, 'Win');
+        } else {
+            // console.log('Lose');
+            generateRow(input, 'Lose');
+        }
+    }
+
+};
+
+erase.onclick = function (event) {
+    event.preventDefault();
+    var table = document.getElementById('statusTable');
+    var tableRows = table.getElementsByTagName('tr');
+    var rowCount = tableRows.length;
+
+    while(--rowCount) {
+        table.deleteRow(rowCount);
+    }
+
+};
+
+
+// Functions
+
+function generateRow(data, status) {
+    var income = data.a_bet,
+        budget = data.budget;
+
+    if (status == 'Win') {
+        income = (income * data.k) - income;
+        budget = budgetCount(data.budget, income, 'win');
+    } else {
+        budget = budgetCount(data.budget, income, 'lose');
+    }
+
+    var table = document.getElementById('statusTable');
+
+    var row = table.insertRow(-1);
+
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+
+    cell1.innerHTML = status;
+    cell2.innerHTML = income;
+    cell3.innerHTML = budget;
+}
+
+function budgetCount(budget, income, status) {
+    if ( typeof budgetCount.counter == 'undefined' ) {
+        budgetCount.counter = budget;
+    }
+
+    if (status == 'win') {
+        budgetCount.counter = +budgetCount.counter + +income;
+    } else {
+        budgetCount.counter = +budgetCount.counter - +income;
+    }
+
+    return budgetCount.counter;
+}
+
+function winCounter(status) {
+    if ( typeof winCounter().wins == 'undefined' ) {
+        winCounter.wins = 0;
+    }
+
+    if ( typeof winCounter().loses == 'undefined' ) {
+        winCounter.loses = 0;
+    }
+    
+    if (status == 'win') {
+        ++winCounter.wins;
+    } else {
+        ++winCounter.loses;
+    }
+}
+
+winCounter.getStats = function() {
+    console.log('Wins: '+ winCounter.wins + ' Loses: ' + winCounter.loses);
+};
